6a27e657705cfadf88c5bc59a603a8386c30c897
stepdav <davitstepanyan99@gmail.com>
2025-04-02 20:22:42 +0400

    class 3

--- core/src/main/java/com/bootcamp/demo/presenters/GameUI.java
+++ core/src/main/java/com/bootcamp/demo/presenters/GameUI.java
@@ -1,15 +1,20 @@
 package com.bootcamp.demo.presenters;
 
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Cell;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.bootcamp.demo.engine.Resources;
 import com.bootcamp.demo.events.core.EventListener;
 import com.bootcamp.demo.events.core.EventModule;
 import com.bootcamp.demo.managers.API;
 import com.bootcamp.demo.pages.core.APage;
+import jdk.internal.loader.Resource;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -44,7 +49,30 @@ public class GameUI extends ScreenAdapter implements Disposable, EventListener {
 
     private void playground () {
         final Table testTable = new Table();
-        rootUI.add(testTable).size(300);
+        final Table testTable2 = new Table();
+        final Table testTable3 = new Table();
+        final Table testTable4 = new Table();
+        final Table testTable5 = new Table();
+
+        final Image gift = new Image(Resources.getDrawable("ui/ui-chat-gift-button-icon", Color.GREEN));
+        gift.setScaling(Scaling.fit);
+
+        final Table playground = new Table();
+        playground.pad(30);
+        playground.defaults().size(300);
+        playground.add(testTable);
+        playground.add(testTable2);
+        playground.row();
+        playground.add(testTable3);
+        playground.add(gift).size(200, 600);
+
+        final Table playground2 = new Table();
+        playground2.setBackground(Resources.getDrawable("basics/white-squircle-35", Color.GREEN));
+        final Image testImage = new Image();
+        testImage.setDrawable(Resources.getDrawable("basics/white-squircle-35", Color.GREEN));
+        playground2.add(testImage).size(300);
+
+        rootUI.add(playground2);
         rootUI.debugAll();
     }
 
